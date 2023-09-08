064e51e2487f0bc854e24f7f9253e31790fae60a
Martin Hegel <martin.hegel@gmail.com>
2023-09-08 15:30:55 +0200

    implement navHost and HomeAdapter

--- app/src/main/java/com/example/abschlussprojekt/adapter/HomeAdapter.kt
+++ app/src/main/java/com/example/abschlussprojekt/adapter/HomeAdapter.kt
@@ -1,4 +1,43 @@
 package com.example.abschlussprojekt.adapter
 
-class HomeAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.net.toUri
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.example.abschlussprojekt.data.models.Pokemon
+import com.example.abschlussprojekt.databinding.ItemHomeBinding
+import com.example.abschlussprojekt.ui.HomeFragmentDirections
+import com.example.abschlussprojekt.ui.SharedViewModel
+
+class HomeAdapter(private val viewModel: SharedViewModel, private val dataSet: List<Pokemon>) :
+    RecyclerView.Adapter<HomeAdapter.ItemHomeViewHolder>() {
+
+    inner class ItemHomeViewHolder(val binding: ItemHomeBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHomeViewHolder {
+        val binding = ItemHomeBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ItemHomeViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return dataSet.size
+    }
+
+    override fun onBindViewHolder(holder: ItemHomeViewHolder, position: Int) {
+        val pokemon = dataSet[position]
+        val imgUri = pokemon.sprites.sprite.toUri().buildUpon().scheme("https").build()
+
+        holder.binding.ivPokemon.load(imgUri)
+        holder.binding.tvPokeId.text = pokemon.id.toString()
+        holder.binding.tvPokeName.text = pokemon.name
+
+        holder.binding.cardview.setOnClickListener {
+            val navController = holder.itemView.findNavController()
+            viewModel.currentPokemon = pokemon
+            navController.navigate(HomeFragmentDirections.actionHomeFragmentToDetailFragment())
+        }
+    }
 }
--- app/src/main/java/com/example/abschlussprojekt/ui/SharedViewModel.kt
+++ app/src/main/java/com/example/abschlussprojekt/ui/SharedViewModel.kt
@@ -1,4 +1,17 @@
 package com.example.abschlussprojekt.ui
 
-class SharedViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.abschlussprojekt.data.models.Pokemon
+
+class SharedViewModel(application: Application) : AndroidViewModel(application) {
+
+   // private val database = getDatabase(application)
+
+    var inputText = MutableLiveData<String>()
+
+    lateinit var currentPokemon: Pokemon
+
 }
